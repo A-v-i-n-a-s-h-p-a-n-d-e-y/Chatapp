0e5f89d1415db833864e37887a73bb49fcdea6f2
unknown <ap358456@gmail.com>
2023-07-04 22:27:19 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBdqEvgStQ7i3fKfxfBEphQDexuiLAYcsY",
+  authDomain: "packprogrammer-chatapp-4c881.firebaseapp.com",
+  projectId: "packprogrammer-chatapp-4c881",
+  storageBucket: "packprogrammer-chatapp-4c881.appspot.com",
+  messagingSenderId: "564400643455",
+  appId: "1:564400643455:web:431ba21e6a0302b5110e4a"
+};
+
+// Initialize Firebase
+export  const app = initializeApp(firebaseConfig);
